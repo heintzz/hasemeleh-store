ebcba301e7e46af43ec0fbc17557d6fc74678168
heintzz <mhasnanr@gmail.com>
2022-07-26 19:58:10 +0700

    adjust responsiveness :>

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Item({ item, addToCart, isLogin }) {
     const { img, title, price, series, id } = item
     return (
-        <div className="flex flex-col sm:w-6/12 lg:w-4/12 p-6 text-sm hover:scale-110 transition duration-700">
+        <div className="flex flex-col w-6/12 lg:w-4/12 pl-6 pt-6 text-[10px] sm:text-sm hover:scale-110 transition duration-700">
             <Link to={`/about/${id}`}>
                 <img
                     className="bg-slate-300 rounded-lg mb-3 w-full"
@@ -14,12 +14,15 @@ export default function Item({ item, addToCart, isLogin }) {
             </Link>
             <div className="mb-2 px-2">
                 <h3>{title}</h3>
-                <h4 className="text-xs">{series}</h4>
+                <h4 className="text-[10px] sm:text-xs">{series}</h4>
             </div>
             <div className="flex justify-between items-center px-2">
                 <p>{`$ ${price}`}</p>
-                <Link to={isLogin ? '' : '/login'} onClick={() => addToCart(id)}>
-                    <img src="./icons/bag-add.svg" />
+                <Link
+                    to={isLogin ? '' : '/login'}
+                    onClick={() => addToCart(id)}
+                >
+                    <img src="./icons/bag-add.svg" className="w-3 sm:w-full" />
                 </Link>
             </div>
         </div>
