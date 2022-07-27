8765bb60d4219244bbb5789b98b2687a9dfb6feb
heintzz <mhasnanr@gmail.com>
2022-07-27 10:00:36 +0700

    add config file

---
+++ src/config/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import key from '../apikey'
+
+const firebaseConfig = {
+    apiKey: key,
+    authDomain: 'e-commerce-24eeb.firebaseapp.com',
+    projectId: 'e-commerce-24eeb',
+    storageBucket: 'e-commerce-24eeb.appspot.com',
+    messagingSenderId: '448110687314',
+    appId: '1:448110687314:web:854b197a1c9767ffe71b8b',
+}
+
+initializeApp(firebaseConfig)
+export const db = getFirestore()
+
+export const auth = getAuth()
