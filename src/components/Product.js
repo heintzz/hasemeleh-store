e13ce716e9966ef08c2c937ea6473d27fb4e8e7f
heintzz <mhasnanr@gmail.com>
2022-09-03 16:15:50 +0700

    fix: back to home while not found bug

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,8 +2,10 @@ import React, { useContext } from 'react'
 import Item from './Item'
 import AppContext from '../context/AppContext'
 import notFound from '../icons/not-found.svg'
+import { useNavigate } from 'react-router-dom'
 
 export default function Product({ keyword }) {
+  let navigate = useNavigate()
   const { items, isLogin, addToCart } = useContext(AppContext)
   const filteredItems = items.filter(
     (item) =>
@@ -35,7 +37,7 @@ export default function Product({ keyword }) {
             <div className="mb-5">Item not found...</div>
             <button
               className="bg-red-300 w-fit py-1 px-2 rounded-xl"
-              onClick={() => window.location.reload()}
+              onClick={() => navigate('/')}
             >
               Back
             </button>
