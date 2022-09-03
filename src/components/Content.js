884123172910acadaa518ce69f3a69149260ea55
heintzz <mhasnanr@gmail.com>
2022-09-03 16:09:41 +0700

    add: useSearchParams

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,32 +3,33 @@ import Product from './Product'
 import Search from './Search'
 import ContentSkeleton from '../skeletons/Products/ContentSkeleton'
 import AppContext from '../context/AppContext'
+import { useSearchParams } from 'react-router-dom'
 
 export default function Content() {
-    const { isLoading } = useContext(AppContext)
-    const [searchKey, setSearchKey] = useState('')
-    // eslint-disable-next-line
-    const [keyword, setKeyword] = useState('')
+  const { isLoading } = useContext(AppContext)
+  const [searchKey, setSearchKey] = useState('')
+  const [searchParams, setSearchParams] = useSearchParams()
 
-    const changeHandler = (e) => {
-        setSearchKey(e.target.value)
+  const query = searchParams.get('search') ? searchParams.get('search') : ''
+  const changeHandler = (e) => {
+    setSearchKey(e.target.value)
+  }
+
+  const enterHandler = (e) => {
+    if (e.key === 'Enter') {
+      setSearchKey('')
+      setSearchParams({ search: searchKey })
     }
+  }
 
-    const enterHandler = (e) => {
-        if (e.key === 'Enter') {
-            setKeyword(searchKey)
-            setSearchKey('')
-        }
-    }
-
-    return (
-        <div className="w-10/12 mt-5 ml-10 md:w-8/12">
-            <Search
-                changeHandler={changeHandler}
-                searchKey={searchKey}
-                enterHandler={enterHandler}
-            />
-            {isLoading ? <ContentSkeleton /> : <Product keyword={keyword} />}
-        </div>
-    )
+  return (
+    <div className="w-10/12 mt-5 ml-10 md:w-8/12">
+      <Search
+        changeHandler={changeHandler}
+        searchKey={searchKey}
+        enterHandler={enterHandler}
+      />
+      {isLoading ? <ContentSkeleton /> : <Product keyword={query} />}
+    </div>
+  )
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -4,55 +4,55 @@ import AppContext from '../context/AppContext'
 import notFound from '../icons/not-found.svg'
 
 export default function Product({ keyword }) {
-    const { items, isLogin, addToCart } = useContext(AppContext)
-    const filteredItems = items.filter(
-        (item) =>
-            item.title.toLowerCase().includes(keyword.toLowerCase()) ||
-            item.types.find((type) => type.includes(keyword.toLowerCase()))
-    )
+  const { items, isLogin, addToCart } = useContext(AppContext)
+  const filteredItems = items.filter(
+    (item) =>
+      item.title.toLowerCase().includes(keyword.toLowerCase()) ||
+      item.types.find((type) => type.includes(keyword.toLowerCase()))
+  )
 
-    return (
-        <div className="flex flex-wrap mt-10">
-            {keyword ? (
-                filteredItems.length ? (
-                    filteredItems.map((item) => {
-                        return (
-                            <Item
-                                item={item}
-                                addToCart={addToCart}
-                                isLogin={isLogin}
-                                key={item.id}
-                            />
-                        )
-                    })
-                ) : (
-                    <div className="sm:mx-auto">
-                        <img
-                            src={notFound}
-                            alt="not found"
-                            className="w-40 mb-10 lg:w-64"
-                        />
-                        <div className="mb-5">Item not found...</div>
-                        <button
-                            className="bg-red-300 w-fit py-1 px-2 rounded-xl"
-                            onClick={() => window.location.reload()}
-                        >
-                            Back
-                        </button>
-                    </div>
-                )
-            ) : (
-                items.map((item) => {
-                    return (
-                        <Item
-                            item={item}
-                            addToCart={addToCart}
-                            isLogin={isLogin}
-                            key={item.id}
-                        />
-                    )
-                })
-            )}
-        </div>
-    )
+  return (
+    <div className="flex flex-wrap mt-10">
+      {keyword ? (
+        filteredItems.length ? (
+          filteredItems.map((item) => {
+            return (
+              <Item
+                item={item}
+                addToCart={addToCart}
+                isLogin={isLogin}
+                key={item.id}
+              />
+            )
+          })
+        ) : (
+          <div className="sm:mx-auto">
+            <img
+              src={notFound}
+              alt="not found"
+              className="w-40 mb-10 lg:w-64"
+            />
+            <div className="mb-5">Item not found...</div>
+            <button
+              className="bg-red-300 w-fit py-1 px-2 rounded-xl"
+              onClick={() => window.location.reload()}
+            >
+              Back
+            </button>
+          </div>
+        )
+      ) : (
+        items.map((item) => {
+          return (
+            <Item
+              item={item}
+              addToCart={addToCart}
+              isLogin={isLogin}
+              key={item.id}
+            />
+          )
+        })
+      )}
+    </div>
+  )
 }
